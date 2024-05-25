fee1c0596e59734f8c177c31f3508f3d653919b7
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-05-25 12:04:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -145,6 +145,6 @@ function updateCartTotal(){
          
     }
     total = Math.round(total * 100 )/100;
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$ '+ total + '.00';
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$ '+ total;
  
 }
